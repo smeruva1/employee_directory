adc894ffc687093b03e8d88b463e24cef2986040
Sudar Meruva <meruva_s@yaho.com>
2020-04-26 07:27:21 -0400

    Added readme and increased employee api fetch count to 200

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default {
     getEmployees: function () {
-        return axios.get('https://randomuser.me/api/?results=50');
+        return axios.get('https://randomuser.me/api/?results=200');
     }
 };
